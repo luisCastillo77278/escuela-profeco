ca567603d70c4e9fd665ea237db675f3d3ebb4c2
luis castillo <lc77278@gmail.com>
2021-06-14 00:17:38 -0500

    add ejs templater.js

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const path = require('path');
 
+
 const app = express();
 
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/inicio.html'));
+    res.render(path.join(__dirname, '/views/inicio.ejs'));
 });
 
 app.get('/conocenos', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/conocenos.html'));
+    res.render(path.join(__dirname, '/views/conocenos.ejs'));
 });
 
 app.get('/aspirantes', (req, res) => {
