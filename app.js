c581d6ad1ffd3a1e6c26d1a9fd451b402e034387
luis castillo <lc77278@gmail.com>
2021-06-13 23:23:27 -0500

    add files views/

--- app.js
+++ app.js
@@ -7,7 +7,23 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
+    res.sendFile(path.join(__dirname, '/views/inicio.html'));
+});
+
+app.get('/conocenos', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/conocenos.html'));
+});
+
+app.get('/aspirantes', (req, res) => {
+
+});
+
+app.get('/alumnos', (req, res) => {
+
+})
+
+app.get('/profesores', (req, res) => {
+
 });
 
 app.listen(3000, () => console.log('Servidor en el puerto 3000'));
