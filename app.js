909db10a685958c19ef132ab12e3ff2c7dde8250
luis castillo <lc77278@gmail.com>
2021-06-26 23:16:36 -0500

    modify app.js

--- app.js
+++ app.js
@@ -5,53 +5,49 @@ const path = require('path');
 const app = express();
 
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-    res.render(path.join(__dirname, '/views/inicio.ejs'));
+    res.sendFile(path.join(__dirname, '/views/inicio.html'));
 });
 
 app.get('/conocenos', (req, res) => {
-    res.render(path.join(__dirname, '/views/conocenos.ejs'));
+    res.sendFile(path.join(__dirname, '/views/conocenos.html'));
 });
 
 app.get('/historia', (req, res) => {
-    res.render(path.join(__dirname, '/views/historia.ejs'));
+    res.sendFile(path.join(__dirname, '/views/historia.html'));
 });
 
 app.get('/objetivos', (req, res) => {
-    res.render(path.join(__dirname, '/views/objetivos.ejs'));
+    res.sendFile(path.join(__dirname, '/views/objetivos.html'));
 });
 
 app.get('/mision-vision', (req, res) => {
-    res.render(path.join(__dirname, '/views/misionvision.ejs'));
+    res.sendFile(path.join(__dirname, '/views/misionvision.html'));
 })
 
 app.get('/aspirantes', (req, res) => {
-    res.render(path.join(__dirname, '/views/aspirantes.ejs'));
+    res.sendFile(path.join(__dirname, '/views/aspirantes.html'));
 });
 
 app.get('/alumnos', (req, res) => {
-    res.render(path.join(__dirname, '/views/alumnos.ejs'));
-})
-
-app.get('/alumnos-sis', (req, res) => {
-    res.render(path.join(__dirname, '/views/alumno-sis.ejs'));
+    res.sendFile(path.join(__dirname, '/views/alumnos.html'));
 })
 
 app.get('/profesores', (req, res) => {
-    res.render(path.join(__dirname, '/views/profesores.ejs'));
+    res.sendFile(path.join(__dirname, '/views/profesores.html'));
 });
 
 app.get('/informes', (req, res) => {
-    res.render(path.join(__dirname, '/views/informes.ejs'));
+    res.sendFile(path.join(__dirname, '/views/informes.html'));
 });
 
 app.get('/preguntas', (req, res) => {
-    res.render(path.join(__dirname, '/views/preguntas.ejs'));
+    res.render(path.join(__dirname, '/views/preguntas.html'));
 });
 
 app.listen(3000, () => console.log('Servidor en el puerto 3000'));
